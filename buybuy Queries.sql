a2e282782c1d9095461d452833a3b5cc1bf12a82
Francis Folaranmi <88105784+DonFrancis1@users.noreply.github.com>
2021-11-05 14:40:23 +0100

    Update buybuy Queries.sql

--- buybuy Queries.sql
+++ buybuy Queries.sql
@@ -17,19 +17,14 @@ SELECT
   [REVENUE] - [COST] AS [Profit] 
 FROM 
   [buybuy].[dbo].[newbuybuy];
-
-
 --1. TIME-PROFIT ANALYSIS
 --1a: The total profit made by BuyBuy from 1Q11 to 4Q16 (all quarters of every year)
 SELECT 
-  SUM([REVENUE]-[COST]) AS [Total Profit] 
+  SUM([REVENUE] - [COST]) AS [Total Profit] 
 FROM 
-  [dbo].[newbuybuy]
+  [dbo].[newbuybuy] 
 WHERE 
   YEAR >= '2011';
-
-
-
 --1b:The total profit made by BuyBuy in Q2 of every year from 2011 to 2016.
 SELECT 
   SUM([REVENUE] - [COST]) AS [Total Profit], 
@@ -96,9 +91,6 @@ WHERE
   AND '2016-06-01' 
 GROUP BY 
   YEAR;
-
-
-
 --1c:The annual profit made by BuyBuy from the year 2011 to 2016.
 SELECT 
   SUM([REVENUE] - [COST]) AS [Total Profit], 
@@ -111,10 +103,6 @@ GROUP BY
   YEAR 
 ORDER BY 
   YEAR;
-
-
-
-
 --2. REGION PROFIT ANALYSIS
 --2a. The countries where BuyBuy has made the most profit and also the least profit of all-time.
 SELECT 
@@ -123,9 +111,9 @@ SELECT
 FROM 
   [dbo].[newbuybuy] 
 GROUP BY 
-  Country;
-
-
+  Country 
+ORDER BY 
+  SUM([REVENUE] - [COST]) DESC;
 --2b. the Top-10 most profitable countries for BuyBuy sales operations from 2011 to 2016
 SELECT 
   TOP 10 SUM([REVENUE]) AS [Sales], 
@@ -136,14 +124,22 @@ WHERE
   YEAR BETWEEN '2011' 
   AND '2016' 
 GROUP BY 
-  Country;
-
-
+  Country 
+ORDER BY 
+  SUM([REVENUE] - [COST]) DESC;
 --2c. The all-time Top-10 least profitable countries for BuyBuy sales operations.
---Refer 2b above
-
-
-
+SELECT 
+  TOP 10 SUM([REVENUE]) AS [Sales], 
+  [Country] 
+FROM 
+  [dbo].[newbuybuy] 
+WHERE 
+  YEAR BETWEEN '2011' 
+  AND '2016' 
+GROUP BY 
+  Country 
+ORDER BY 
+  SUM([REVENUE] - [COST]) ASC;
 --3. PRODUCT REVENUE ANALYSIS
 --3a. Ranking all product categories sold by Buybuy, from least amount to the most amount of all-time revenue generated.
 SELECT 
@@ -154,28 +150,25 @@ FROM
 GROUP BY 
   [Product Category] 
 ORDER BY 
-  [Product Category];
-
-
+  SUM([REVENUE]) ASC;
 --3b. Top-2 product categories offered by Buybuy with an all-time high number of units sold.
 SELECT 
-  SUM([Order Quantity]) AS [Unit Sold], 
+  TOP 2 SUM([Order Quantity]) AS [Unit Sold], 
   [Product Category] 
 FROM 
   [dbo].[newbuybuy] 
 GROUP BY 
   [Product Category] 
 ORDER BY 
-  [Product Category];
-
-
+  SUM([Order Quantity]) DESC;
 --3c. Top 10 highest-grossing products sold by BuyBuy based on all-time profits.
 SELECT 
-  SUM([Order Quantity]) AS [Unit Sold], 
+  TOP 10 SUM([Order Quantity]) AS [Unit Sold], 
   [Product] 
 FROM 
   [dbo].[newbuybuy] 
 GROUP BY 
   [Product] 
 ORDER BY 
-  [Product];
+  SUM([Order Quantity]) DESC;
+
